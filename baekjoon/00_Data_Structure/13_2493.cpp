35ad7d559527eec058ba910862bc2fe28213ebfc
DohyeokKim <hyeokdokim@gmail.com>
2022-12-15 13:54:08 +0900

    [00] Solve 2493 in baekjoon
    - [2493](https://www.acmicpc.net/source/52690536)

---
+++ baekjoon/00_Data_Structure/13_2493.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Tower{
+    int number;
+    int height;
+    Tower(int _number, int _height) : number(_number), height(_height) {}
+};
+int main() {
+    // ios::sync_with_stdio(false); cin.tie(NULL);
+    int N{0};
+    cin >> N;
+
+    int heights[N + 1], received[N + 1];
+    heights[1] = 0;
+    received[1] = 0;
+
+    stack<Tower> s;
+
+    for(int i {1}; i <= N; i++) {
+        int curHeight{0};
+        cin >> curHeight;  
+
+        while(!s.empty()) {
+            if(s.top().height < curHeight) 
+                s.pop();
+            else if(s.top().height > curHeight) {
+                cout << s.top().number << ' ';
+                break;
+            }
+        }
+        if(s.empty()) cout << '0' << ' ';
+        s.push(Tower(i, curHeight));
+    }
+
+}
